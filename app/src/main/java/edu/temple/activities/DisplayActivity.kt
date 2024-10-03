6480739b09b55ec5c375a9e7bc0bb0007d3bbf0a
joue-zero <elnagaryoussef864@gmail.com>
2024-10-03 14:57:14 -0400

    Upload The Main Files

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,14 +1,32 @@
 package edu.temple.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class DisplayActivity : AppCompatActivity() {
 
     // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
-
+//    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+//        if (result.resultCode == Activity.RESULT_OK) {
+//            val data: Intent? = result.data
+//            val textSize = data?.getIntExtra("textSize", 0)
+//            lyricsDisplayTextView.textSize = textSize!!.toFloat()
+//        }
+//    }
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = it.data
+            val textSize = data?.getIntExtra("textSize", 0)
+            lyricsDisplayTextView.textSize = textSize!!.toFloat()
+            // the  !! operator is used to assert that the value is not null
+            // the data?.getIntExtra() method returns null if the key is not found
+        }
+    }
     // TODO Step 3: Use returned value for lyricsDisplayTextView text size
 
     private lateinit var lyricsDisplayTextView: TextView
@@ -20,6 +38,11 @@ class DisplayActivity : AppCompatActivity() {
 
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
         textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
+
+        textSizeSelectorButton.setOnClickListener {
+            val intent = Intent(this, TextSizeActivity::class.java)
+            launcher.launch(intent)
+        }
 
     }
 }
